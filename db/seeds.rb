2f3c2ccc5fd499ba0d161cedafa5ca2b85a03975
Letizia Ackaouy <ackaouyletizia@gmail.com>
2019-02-21 08:40:36 +0000

    one big commit which I will never repeat

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+puts 'Creating 5 fake restaurants...'
+Restaurant.destroy_all
+5.times do
+  restaurant = Restaurant.create!(
+    name:    Faker::GreekPhilosophers.name ,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    category:  ["chinese", "italian", "japanese", "french", "belgian"].sample
+    )
+end
+puts 'Finished!'
